e1bf0d8fc747a8e4ce7dac6b38fde657b74cda95
rawanshisht <rawan.shisht@gmail.com>
2024-08-12 16:08:12 +0100

    edit files

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,30 +1,29 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import todoListRoute from "../routes/todoListRoute.js";
 import todoListItemRoute from "../routes/todoListItemRoute.js";
 import authRoute from "../routes/authRoute.js";
 import cors from "cors";
-import dotenv from "dotenv";
 import { protect } from "../middlewares/authMiddleware.js";
+
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  console.log(req);
-  return res.status(200).send("Welcome");
+  res.send("Welcome");
 });
+
 app.use("/api/todolists", protect, todoListRoute);
 app.use("/api/todos", protect, todoListItemRoute);
 app.use("/api/users", authRoute);
 
 mongoose
   .connect(process.env.mongoDBURL)
-  .then(() => {
-    console.log("App connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => console.log("App connected to DB"))
+  .catch((err) => console.error("DB connection error:", err));
+
 export default app;
