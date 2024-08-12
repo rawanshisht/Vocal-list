adf8ec9cf92847bc080f095947a0245f0fa93538
rawanshisht <rawan.shisht@gmail.com>
2024-08-12 13:55:20 +0100

    fix favicon issue

--- Backend/index.js
+++ Backend/index.js
@@ -6,11 +6,20 @@ import authRoute from "./routes/authRoute.js";
 import cors from "cors";
 import dotenv from "dotenv";
 import { protect } from "./middlewares/authMiddleware.js";
+import favicon from "serve-favicon";
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+// Error handling middleware to ignore favicon requests
+app.use((req, res, next) => {
+  if (req.originalUrl === "/favicon.ico") {
+    res.status(204).end(); // No Content
+  } else {
+    next();
+  }
+});
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(200).send("Welcome");
