d9a899ef9d943422165a49ea088fcaf2f559018e
rawanshisht <rawan.shisht@gmail.com>
2024-08-13 00:13:46 +0100

    deploy backend settings

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -6,13 +6,15 @@ import todoListItemRoute from "../routes/todoListItemRoute.js";
 import authRoute from "../routes/authRoute.js";
 import cors from "cors";
 import { protect } from "../middlewares/authMiddleware.js";
-
+import path from "path";
+import favicon from "serve-favicon";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(favicon(path.join(path.resolve(), "public", "favicon.ico")));
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
@@ -22,7 +24,11 @@ app.use("/api/todos", protect, todoListItemRoute);
 app.use("/api/users", authRoute);
 
 mongoose
-  .connect(process.env.mongoDBURL)
+  .connect(process.env.mongoDBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000,
+  })
   .then(() => console.log("App connected to DB"))
   .catch((err) => console.error("DB connection error:", err));
 
